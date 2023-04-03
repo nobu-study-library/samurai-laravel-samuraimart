1484eef9520ad1207c21851ce1432fd6b85ee942
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-03 12:17:30 +0900

    💄 商品の追加画面のスタイル変更

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,33 +1,34 @@
-<div>
-  <h2>Add New Product</h2>
-</div>
-<div>
-  <a href="{{ route('products.index') }}">Back</a>
-</div>
+@extends('layouts.app')
 
-<form action="{{ route('products.store') }}" method="post">
-  @csrf
-  <div>
-    <strong>Name：</strong>
-    <input type="text" name="name" placeholder="Name">
+@section('content')
+  <div class="container">
+    <h1>新しい商品を追加</h1>
+
+    <form action="P{{ route('products.store') }}" method="post">
+      @csrf
+      <div class="form-group">
+        <label for="productName">商品名</label>
+        <input type="text" name="name" id="productName" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="productDescription">商品説明</label>
+        <textarea name="description" id="productDescription" class="form-control"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="productPrice">価格</label>
+        <input type="number" name="price" id="productPrice" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="productCategory">カテゴリ</label>
+        <select name="categoryId" id="productCategory" class="form-control">
+          @foreach ($categoryList as $category)
+            <option value="{{ $category->id }}">{{ $category->name }}</option>
+          @endforeach
+        </select>
+      </div>
+      <button type="submit" class="btn btn-success">商品を登録</button>
+    </form>
+
+    <a href="{{ route('products.index') }}">商品一覧に戻る</a>
   </div>
-  <div>
-    <strong>Description：</strong>
-    <textarea style="height:150px" name="description" placeholder="Description"></textarea>
-  </div>
-  <div>
-    <strong>Price：</strong>
-    <input type="number" name="price" placeholder="Price">
-  </div>
-  <div>
-    <strong>Category：</strong>
-    <select name="category_id">
-      @foreach ($categoryList as $category)
-        <option value="{{ $category->id }}">{{ $category->name }}</option>
-      @endforeach
-    </select>
-  </div>
-  <div>
-    <button type="submit">Submit</button>
-  </div>
-</form>
+@endsection
