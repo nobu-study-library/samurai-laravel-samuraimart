0027ecde57a29c6d544808c08f68a484b948732b
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-03 13:09:31 +0900

    💄 商品更新ページのスタイル調整

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,38 +1,39 @@
-<div>
-  <h2>Edit Product</h2>
-</div>
-<div>
-  <a href="{{ route('products.index') }}">Back</a>
-</div>
+@extends('layouts.app')
 
-<form action="{{ route('products.update', $product->id) }}" method="post">
-  @csrf
-  @method('PUT')
-  <div>
-    <strong>Name：</strong>
-    <input type="text" name="name" value="{{ $product->name }}" placeholder="Name">
+@section('content')
+  <div class="container">
+    <h1>商品情報更新</h1>
+    <form action="{{ route('products.update', $product->id) }}" method="post">
+      @csrf
+      @method('put')
+
+      <div class="form-group">
+        <label for="productName">商品名</label>
+        <input type="text" name="name" id="productName" class="form-control" value="{{ $product->name }}">
+      </div>
+      <div class="form-group">
+        <label for="productDescription">商品説明</label>
+        <textarea name="description" id="productDescription" class="form-control">{{ $product->description }}</textarea>
+      </div>
+      <div class="form-group">
+        <label for="productPrice">価格</label>
+        <input type="number" name="price" id="productPrice" class="form-control" value="{{ $product->price }}">
+      </div>
+      <div class="form-group">
+        <label for="product-category">カテゴリ</label>
+        <select name="category_id" class="form-control" id="product-category">
+          @foreach ($categoryList as $category)
+            @if ($category->id === $product->category_id)
+              <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+            @else
+              <option value="{{ $category->id }}">{{ $category->name }}</option>
+            @endif
+          @endforeach
+        </select>
+      </div>
+      <button type="submit" class="btn btn-danger">更新</button>
+    </form>
+
+    <a href="{{ route('products.index') }}">商品一覧に戻る</a>
   </div>
-  <div>
-    <strong>Description：</strong>
-    <textarea name="height:150px" name="description" placeholder="description">{{ $product->description }}</textarea>
-  </div>
-  <div>
-    <strong>Price:</strong>
-    <input type="number" name="price" value="{{ $product->price }}">
-  </div>
-  <div>
-    <strong>Category：</strong>
-    <select name="categoryId">
-      @foreach ($categoryList as $category)
-        @if ($category->id === $product->category_id)
-          <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
-        @else
-          <option value="{{ $category->id }}">{{ $category->name }}</option>
-        @endif
-      @endforeach
-    </select>
-  </div>
-  <div>
-    <button type="submit">Submit</button>
-  </div>
-</form>
+@endsection
