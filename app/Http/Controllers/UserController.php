2c65946425e9f9a159a2fba304c7abe6310a561b
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-07 13:20:20 +0900

    ✨ ユーザー編集機能の追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
   public function edit(User $user)
   {
     $user = Auth::user();
-    return view('users,edit', compact('user'));
+    return view('users.edit', compact('user'));
   }
 
   /**
