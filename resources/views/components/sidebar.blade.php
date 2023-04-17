b5a1edb61ce3adbe4e4c58904d8999f357e7219a
Nobu <86172592+bunon0@users.noreply.github.com>
2023-04-17 18:36:25 +0900

    ✨ 商品一覧ページの親カテゴリ反映の修正

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,7 +2,7 @@
   @foreach ($majorCategories as $majorCategory)
     <h2>{{ $majorCategory->name }}</h2>
     @foreach ($categories as $category)
-      @if ($category->major_category_id === $major_category->id)
+      @if ($category->major_category_id === $majorCategory->id)
         <label class="samuraimart-sidebar-category-label">
           <a href="#">{{ $category->name }}</a>
         </label>
